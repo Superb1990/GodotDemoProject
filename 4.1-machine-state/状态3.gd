29dcc0f4106732fffb945e58b10652e0aa0d791e
superb1990 <550341369@qq.com>
2026-05-18 22:43:28 +0800

    4.4 第一种  状态机 完全体（未优化）

--- 4.1-machine-state/状态3.gd
+++ 4.1-machine-state/状态3.gd
@@ -2,13 +2,11 @@ extends Node
 
 
 func 开始时():
-	get_parent().modulate = Color(1.0, 0.0, 0.0, 1.0)
+	get_parent().modulate = Color(1.0, 1.0, 0.0, 1.0)
 	pass
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_left"):
-		get_parent().状态改变(load("res://状态2.gd"))
-	
+		get_parent().状态改变(load("res://状态1.gd"))
 	if Input.is_action_just_pressed("ui_right"):
-		get_parent().状态改变(load("res://状态3.gd"))
-		
+		get_parent().状态改变(load("res://状态2.gd"))
